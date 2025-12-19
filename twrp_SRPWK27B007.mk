80b4278cce3bfdea812cce30e3835203338052a6
Ajking420 <apurbahazra420@gmail.com>
2025-12-20 00:16:28 +0530

    Update and rename omni_SRPWK27B007.mk to twrp_SRPWK27B007.mk

--- twrp_SRPWK27B007.mk
+++ twrp_SRPWK27B007.mk
@@ -8,13 +8,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from SRPWK27B007 device
 $(call inherit-product, device/generic/SRPWK27B007/device.mk)
 
 PRODUCT_DEVICE := SRPWK27B007
-PRODUCT_NAME := omni_SRPWK27B007
+PRODUCT_NAME := twrp_SRPWK27B007
 PRODUCT_BRAND := generic
 PRODUCT_MODEL := Generic Device
 PRODUCT_MANUFACTURER := generic
@@ -25,4 +25,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="m15xnndxx-user 13 TP1A.220624.014 E156BXXS7BYH1 release-keys"
 
 BUILD_FINGERPRINT := 
+
 
